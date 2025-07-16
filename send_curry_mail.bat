90bcfea7366c9646804521a854a84528c648648c
surabhi Jeevansai <surabhijeevansai@gmail.com>
2025-07-16 22:22:34 +0530

    send mail

--- send_curry_mail.bat
+++ send_curry_mail.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:\Users\JeevanSai\OneDrive - Stylosoft LLP\Desktop\pratice.js\pratice.js\Team_Currys"
-echo --- [%DATE% %TIME%] Running daily_mail.py --- >> curry_log.txt
-C:\Python313\python.exe daily_mail.py >> curry_log.txt 2>&1
+echo --- [%DATE% %TIME%] Running reminder.py --- >> curry_log.txt
+C:\Python313\python.exe reminder.py >> curry_log.txt 2>&1
 echo --- Finished --- >> curry_log.txt
